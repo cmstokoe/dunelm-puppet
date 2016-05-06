545a94bc901409fa3bf308d4912d89537935d2ae
Chris Stokoe <chris@dunelm.net>
2016-05-06 21:33:08 +0000

    Modifying to account for the change from init to systemd

--- modules/local/transmission/manifests/init.pp
+++ modules/local/transmission/manifests/init.pp
@@ -6,19 +6,30 @@ class transmission {
 			'transmission-daemon',
   		]:
   		ensure  => 'installed',
-	} 
+	} -> 
 
-	exec { 'set_cap':
-    		command => '/sbin/setcap cap_net_bind_service=+ep /usr/bin/transmission-daemon',
-  	}	
-
-	file { '/etc/transmission-daemon/settings.json':
+	file { '/home/dunelm/.config/transmission-daemon/settings.json':
 		content => template('transmission/settings.erb'),
-    		owner => debian-transmission,
-    		group => debian-transmission,
+    		owner => dunelm,
+    		group => users,
     		mode => '600',
 		notify => Service['transmission-daemon']
-  	} 
+  	} ->
+
+	file_line { 'change_user_systemd':
+                path => '/etc/systemd/system/multi-user.target.wants/transmission-daemon.service',
+                line => 'User=dunelm',
+                match => '^User=*',
+                notify => Service['transmission-daemon']
+        } ->
+
+ 	exec { 'sysctl_reload':
+                command => '/bin/systemctl daemon-reload',
+        } ->
+
+ 	exec { 'set_cap':
+                command => '/sbin/setcap cap_net_bind_service=+ep /usr/bin/transmission-daemon',
+        }
 
 	service { 'transmission-daemon':
     		enable => true,
